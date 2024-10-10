1d29594705fc9ac5f15d4d53151dcc5e7ef7a08e
adenohitu <49752462+adenohitu@users.noreply.github.com>
2024-10-10 10:12:43 +0900

    add: Return header Infomations

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,11 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		let requestHeaders = JSON.stringify([...request.headers], null, 2);
+		// console.log(request.headers);
+		// const clientIP = request.headers.get('CF-Connecting-IP');
+
+		console.log(requestHeaders);
+
+		return new Response(requestHeaders);
 	},
 } satisfies ExportedHandler<Env>;
