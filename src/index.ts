6a6883a3b414006cc5859c86c2fdffa052f042c3
adenohitu <49752462+adenohitu@users.noreply.github.com>
2024-10-10 20:56:14 +0900

    fix: return ip

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		let requestHeaders = JSON.stringify([...request.headers], null, 2);
+		// let requestHeaders = JSON.stringify([...request.headers], null, 2);
 		// console.log(request.headers);
 		// const clientIP = request.headers.get('CF-Connecting-IP');
 
-		console.log(requestHeaders);
-		return Response.json([...request.headers]);
+		// console.log(requestHeaders);
+		const data = { ip: request.headers.get('cf-connecting-ip') };
+		return Response.json(data);
 	},
 } satisfies ExportedHandler<Env>;
